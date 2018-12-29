1410159dee058aeadd1487eecb9a3dee789b64f5
Jared Moore <jlcmoore@users.noreply.github.com>
2018-12-29 08:48:32 -0500

    threaded and district based lookup

--- pca/pca_scraper.py
+++ pca/pca_scraper.py
@@ -2,29 +2,53 @@ import mechanize
 import lxml.html
 import sys
 import os
+import multiprocessing
+import socket
 
-url = "http://www.censusindia.gov.in/pca/default.aspx"
+"""
+A utility to scrape the government of India census website.
 
-def get_state(state_id, mode):
+"""
+
+URL = "http://www.censusindia.gov.in/pca/pca.aspx"
+HEADERS = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+RETRIES = 3
+
+def set_form_attribute(form, attribute, value, br):
+    br.select_form(form)
+    br.set_all_readonly(False)
+    br[attribute] = [value]
+    response = br.submit()
+    return response.read()
+
+def mechanize_select_state(state_id, br):
+    br.addheaders = HEADERS
+    response = br.open(URL)
+    rdata = response.read()
+    return set_form_attribute("form1", "StateDropDownList1", state_id, br)
+
+def get_district_ids(state_id):
     br = mechanize.Browser()
-    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-    response = br.open(url)
-    rdata = response.read()
-    br.select_form("form1")
-    br.set_all_readonly(False)
-    br["StateDropDownList1"] = [state_id]
-    response = br.submit()
-    rdata = response.read()
-    br.select_form("form1")
-    br.set_all_readonly(False)
-    br["DistDropDownList1"]=['1']
-    response = br.submit()
-    rdata = response.read()
+    rdata = mechanize_select_state(state_id, br)
+    root = lxml.html.fromstring(rdata)
+    options =  root.xpath('//select[@id="DistDropDownList1"]')[0]
+    districts = []
+    for district in options:
+        value = district.get('value')
+        # some of the options are for all of India or other
+        if len(value) == 3:
+            districts.append(value)
+
+    return districts
+
+def get_district(state_id, district_id, mode):
+    br = mechanize.Browser()
+    mechanize_select_state(state_id, br)
+    rdata = set_form_attribute("form1", "DistDropDownList1", district_id, br)
     root = lxml.html.fromstring(rdata)
     table = root.xpath('//table[@id="GridView1"]')
     lines = []
     if mode == 'header':
-        #tdxpath = './/th'
         row = table[0].xpath('.//tr')[1]
         column = []
         prefix = 'Gridview1_ctl02_lbl_'
@@ -45,26 +69,65 @@ def get_state(state_id, mode):
             lines.append(",".join(column))
     return lines
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1 and sys.argv[1] == 'header':
-        states = range(1)
-        mode = 'header'
-    else:
-        states = range(35)
-        mode = 'data'
-        
+def write_district_census(state_id, district_id, mode):
+    print 'start state %s district %s' % (state_id, district_id)
+    fname = state_id + '_' + district_id + '.csv' if mode == 'data' else 'header.csv'
+    try:
+        if os.path.getsize(fname) > 0:
+            print >>sys.stderr, "found %s, skipping" % fname
+            return
+    except:
+        pass
+    with open(fname, "w") as f:
+        lines = None
+        for i in range(RETRIES):
+            try:
+                lines = get_district(state_id, district_id, mode)
+                break
+            except socket.error as e:
+                # connection reset by peer, probably
+                pass
 
-    for i in states:
+        if lines and len(lines):
+            f.write('\n'.join(lines) + '\n')
+            print >>sys.stderr, "\twrote %d lines in %s" % (len(lines), fname)
+
+def district_thread_wrapper(item):
+    write_district_census(item[0], item[1], 'data')
+
+def state_thread_wrapper(state_id):
+    district_ids = get_district_ids(state_id)
+    districts = []
+    for district_id in district_ids:
+        districts.append((state_id, district_id))
+    return districts
+
+def main():
+    if len(sys.argv) > 1 and sys.argv[1] == 'header':
+        write_district_census("01", "0", 'header')
+        return
+
+    mode = 'data'
+    pool = multiprocessing.Pool(processes=4)
+    state_ids = []
+
+    for i in range(35):
         state_id = '%02d' % (i + 1)
-        fname = state_id + '.csv' if mode == 'data' else 'header.csv'
-        try:
-            if os.path.getsize(fname) > 0:
-                print >>sys.stderr, "found %s, skipping" % fname
-                continue
-        except:
-            pass
-        with open(fname, "w") as f:
-            lines = get_state(state_id, mode)
-            if len(lines):
-                f.write('\n'.join(lines) + '\n')
-                print >>sys.stderr, "wrote %d lines in %s" % (len(lines), fname)
+        state_ids.append(state_id)
+
+    results = pool.map(state_thread_wrapper, state_ids)
+
+    districts_with_state = []
+    for result in results:
+        districts_with_state += result
+
+    print("done querying for district ids")
+    print(districts_with_state)
+
+    pool.map(district_thread_wrapper, districts_with_state)
+
+    pool.close()
+    pool.join()
+
+if __name__ == '__main__':
+    main()
